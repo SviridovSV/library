b9f6f5328eae749aac03677fa76fe8bb34beaf89
SviridovSV <sviridov.sv82@gmail.com>
2017-05-15 09:15:42 +0300

    refactoring two methods

--- main.rb
+++ main.rb
@@ -12,11 +12,12 @@ chak = Reader.new("Chak", "jjj@i.ua")
 lib << chak
 
 rob = Book.new("Rob Roy", "V Scott")
-# puts lib.often_takes_book(rob)
+puts lib.often_takes_book(rob)
 
-# puts lib.most_popular_book(3)
- puts lib.how_many_people_ordered_one_of_three_most_popular_books
+puts lib.most_popular_book(3)
+
+puts lib.how_many_people_ordered_one_of_three_most_popular_books
 
 
-# lib.save_to_file("library.yaml")
+lib.save_to_file("library.yaml")
 
